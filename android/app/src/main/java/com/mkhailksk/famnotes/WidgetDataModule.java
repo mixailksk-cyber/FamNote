75bfdfa37969225ab5a8b1a597798dc4e0a58049
mixailksk-cyber <mixailksk@gmail.com>
2026-03-17 14:58:33 +0500

    Update WidgetDataModule.java

--- android/app/src/main/java/com/mkhailksk/famnotes/WidgetDataModule.java
+++ android/app/src/main/java/com/mkhailksk/famnotes/WidgetDataModule.java
@@ -1,4 +1,4 @@
-package com.mkhailksk.famnotes; // Изменено с famnote на famnotes
+package com.mkhailksk.famnotes;  // Должно быть famnotes
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -7,19 +7,12 @@ import android.util.Log;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.ReadableArray;
-import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.bridge.ReadableMapKeySetIterator;
 import com.facebook.react.module.annotations.ReactModule;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 @ReactModule(name = WidgetDataModule.NAME)
 public class WidgetDataModule extends ReactContextBaseJavaModule {
     public static final String NAME = "WidgetDataModule";
-    private static final String PREFS_NAME = "com.mkhailksk.famnotes.widget"; // Изменено
+    private static final String PREFS_NAME = "com.mkhailksk.famnotes.widget";  // ИСПРАВЛЕНО
     private static final String NOTES_KEY = "widget_notes";
 
     public WidgetDataModule(ReactApplicationContext reactContext) {
@@ -38,7 +31,7 @@ public class WidgetDataModule extends ReactContextBaseJavaModule {
             SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
             prefs.edit().putString(NOTES_KEY, notesJson).apply();
 
-            // Обновляем виджет
+            // ИСПРАВЛЕНО: используем правильный package name
             Class<?> widgetClass = Class.forName("com.mkhailksk.famnotes.widget.NotesWidget");
             java.lang.reflect.Method method = widgetClass.getMethod("updateWidgetNotes", Context.class, String.class);
             method.invoke(null, context, notesJson);
